9b6f8d7c935bda78f2f31f251734cbea12933195
Kirstin Charters <kirstin.charters@otago.ac.nz>
2024-02-04 09:01:17 +1300

    limelight cards

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,24 @@
 
 import { useDatabaseList } from 'vuefire'
 import { ref as dbRef, } from "firebase/database";
-import { db} from './firebase'
+import { db } from './firebase'
 const todos = useDatabaseList(dbRef(db, 'members'))
 
 </script>
 
 <template>
-  <div v-for="todo in todos" :key="todo.id">
-    <div class="card" style="width: 18rem;">
-      <img :src=todo.image class="card-img-top" alt="...">
+  <div v-for="todo in todos" :key="todo.id" class="img__wrap">
+    <div class="card">
+      <img :src="'./src/assets/Website-skins/' + todo.name + '.png'" class="card-img-top img__img" alt="...">
       <div class="card-body">
-        <h5 class="card-title">{{todo.name}}</h5>
-        <div v-for="link in todo.links.split(',')" >
-        <a :href="link" class="btn btn-primary">{{link}}</a>
-      </div>
+        <h5 class="card-title">{{ todo.name }}</h5>
+        <div class="img__description_layer">
+          <div v-for="link in todo.links.split(',')">
+            
+           <a :href="link" class="img__description">{{ link }}</a> 
+          
+          </div>
+        </div>
       </div>
     </div>
   </div>
