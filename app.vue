ebfdcf16f25ae9f44ca3256ee42ae02704fcf864
Kirstin Charters <kirstin.charters@otago.ac.nz>
2024-02-08 08:56:30 +1300

    working things

--- app.vue
+++ app.vue
@@ -15,62 +15,25 @@ export default {
 }
 </script>
 <template>
-  <div v-for="members in myJson">
-    <div v-for="names in members" class="img__wrap">
-      <div class="card">
-        <img :src="'../images/' + names.name + '.png'" class="card-img-top img__img" alt="...">
+  <div class="container">
+  <div v-for="members in myJson" class="row">
+    <div v-for="names in members" class="col-sm-3" >
+      <div class="card " style="width: 18rem;">
+        <img :src="'../images/' + names.name + '.png'" class="card-img-top" alt="...">
         <div v-if="names.enabled == true" class="card-body">
           <h5 class="card-title">{{ names.name }}</h5>
-          <div v-for="link in splitJoin(names.links)" class="img__description_layer">
-            <a :href="link" class="row img__description">{{ link }}</a>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <div v-for="link in splitJoin(names.links)">
+            <a :href="link" class="">{{ link }}</a>
           </div>
         </div>
       </div>
+</div>
+</div>
     </div>
-  </div>
 </template>
 
 <style type="css" scoped>
 /* relevant styles */
-.img__wrap {
-  position: relative;
-  display: inline-block;
-}
-
-.img__img {
-  vertical-align: bottom;
-}
-
-.img__description_layer {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: rgba(196, 201, 231, 0.6);
-  color: #fff;
-  visibility: hidden;
-  opacity: 0;
-  display: block;
-  align-items: center;
-  justify-content: center;
-
-  /* transition effect. not necessary */
-  transition: opacity .2s, visibility .2s;
-}
-
-.img__wrap:hover .img__description_layer {
-  visibility: visible;
-  opacity: 1;
-}
-
-.img__description {
-  transition: .2s;
-  transform: translateY(1em);
-  text-align: center;
-}
-
-.img__wrap:hover .img__description {
-  transform: translateY(0);
-}
 </style>
