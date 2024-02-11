56fb75ee4a4ff1979e7468b7a45d6360f2489200
Kirstin Charters <kirstin.charters@otago.ac.nz>
2024-02-12 11:56:48 +1300

    member update and build files

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,14 +1,1 @@
 // This is all you.
-import '../css/app.css';
-
-// header js
-document.addEventListener("DOMContentLoaded", () => {
-    window.scroll(function(){
-      let winTop = window.scrollTop();
-      if(winTop >= 30){
-        $("body").classList.add("sticky-header");
-      }else{
-        $("body").classList.remove("sticky-header");
-      }
-    });
-  });
